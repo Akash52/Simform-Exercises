7723d3ebcff7776797a3b25ef5e6fdcb79a66ad9
Akash52 <ac8572611@gmail.com>
2022-01-13 14:11:18 +0530

    JS variables & Data types

---
+++ 03_JS/01-Variables/script.js
@@ -0,0 +1,158 @@
+var variableName = 'Welcome to  Variable'
+//For Log
+// console.log(variableName)
+
+//Multi word vars
+
+// var firtsName='Akash'; //Camel case
+// var first_name='Sara';  //Underscore
+// var FirstName='Tome';   // Pascal case
+// var firstname; //Not recomneded 
+
+
+//var,let,const
+
+// var name="Akash Doe"
+// console.log(name)
+// name="sky"           //Re-assing variable
+// console.log(name)
+
+//LET
+
+// let name;
+// let name = "Akash Doe"; 
+// console.log(name);
+// name="Sky";  //Re-assing variable
+// console.log(name);
+
+//Const
+
+// const name="Akash Doe";
+// console.log(name);
+// name="Steave smith"; // can't Re-assing variable
+// const greeting;
+
+
+// Data types
+
+//PRIMITIVE
+
+/*----String--*/
+
+// const exampleString = 'Hello World!'
+// console.log(exampleString)
+
+//Note
+
+/*
+
+    /*
+    In JavaScript There are three types of string :
+    1. Single Qoute
+    2. Double Quote
+    3. Backtick
+*/
+
+//1.Single Quote String
+
+// const singleQuotes = 'Hello!'
+// console.log(singleQuotes)
+
+//2.Double Quotes String
+
+// const doubleQuotes = 'Hello!'
+// console.log(doubleQuotes)
+
+//3. Template String
+
+// const name = 'Jhone'
+// const templateString = `Hello! ${name}`
+// console.log(typeof templateString)
+
+/*----Numbers--*/
+
+// const whole_Num = 5000;
+// const Decimal_Num = 0.55;
+
+// console.log(whole_Num)
+// console.log(Decimal_Num)
+
+
+//We can perform variuos arithmetic Operation
+
+// const result=whole_Num/Decimal_Num;
+// console.log(result)
+
+//NaN - Not an Number
+
+// const string='Hello'
+// const number=10;
+
+// const output=string/number;
+// console.log(output)
+
+/*---Booleans----*/
+
+//true - Yes , Correct, 1
+//false - No , ICorrect, 0
+
+// const isCool=true
+// console.log(typeof isCool)
+
+// if(isCool){
+//     console.log("Hi man , you're cool")
+// }else{
+//     console.log('Oh,hi...')
+// }
+
+/*---Null----*/
+
+// const age = null
+// console.log(typeof age)
+
+//Output : Object
+
+/*---Undefined----*/
+
+/*
+    if variable is declare but we can not assign value then it return undefined
+*/
+
+//REFERENCE
+
+//Object literal
+
+//Object is used for store collection of data and more entities
+
+// const address = {
+//     city: 'Rajkot',
+//     state: 'GUj'
+// }
+
+//Using dot notation we access particular property
+// console.log(typeof address.city)
+// console.log(typeof address.state)
+
+//Array
+
+// const hobbies = ['movie', 'music'];
+// console.log(hobbies)
+// console.log(typeof hobbies)
+//Output - Object
+
+//Date
+
+// const date = new Date()
+// console.log(typeof date)
+//Output - Object
+
+
+/*----JavaScript is Dynamically typed Language ---*/
+
+
+
+
+
+
+
+
