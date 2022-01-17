e0ae179cf9afc9bfeeb50d6b9dcdb553f7a1a841
Akash52 <ac8572611@gmail.com>
2022-01-17 13:14:14 +0530

    functions in JS

---
+++ 03_JS/04-functions/script.js
@@ -0,0 +1,69 @@
+//Function
+
+//A BLOCK OF CODE => Perform A Task 
+
+//Function declaration
+
+// function Square(number){
+//     return number*number;
+// }
+
+//Function Call (calling a function/executing a function)
+
+// const result =Square(20);
+// console.log(result)
+
+//FUNCTION EXRESSION
+
+// const name = function (params) {
+//   //statements
+// }
+
+//An arrow function
+
+// const name = (params) => {
+//   //statements
+// }
+
+// function sayHi(name) {
+//   console.log(`Hi, ${name}!`)
+// }
+// sayHi('Akash')
+// sayHi('Sky')
+
+// Function Return
+
+// function add(a, b) {
+// return 'RETURNED'  <- alwasy run first return statement
+//   return a + b 
+// }
+// const sum = add(2, 3)
+// console.log(sum)
+
+//Arrow Funaction
+
+// const square = (number) => {
+//   return number * number
+// }
+// const result = square(5)
+// console.log(result)
+
+//Simple way write
+
+// const calcArea = (radius) => 3.14 * radius ** 2
+
+//Single Expression
+// var add = (a, b) => a + b
+// //Single Argument
+// var odd = (n) => n % 2
+// //No Arguments
+// var random = () => Math.random()
+// //Multiple Expressions
+// var shout = (s) => {
+//   s = s.toUpperCase()
+//   s = s + '!'
+//   return s
+// }
+
+
+
