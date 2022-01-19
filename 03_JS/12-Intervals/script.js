494f3a561ec158723daf1921cc2b9710a4812273
Akash52 <ac8572611@gmail.com>
2022-01-19 08:44:49 +0530

    Intervals in JS

---
+++ 03_JS/12-Intervals/script.js
@@ -0,0 +1,16 @@
+//Asynchronous Code 
+
+//setInterval
+
+// setInterval(()=>{
+//     console.log('Hello World')
+// },1000)
+
+// const myInterval = setInterval(()=>console.log('Hello wrodl'),1000)
+// clearInterval(myInterval)
+
+// const myInterval = setTimeout(()=>{
+//     console.log("Akash");
+// },2000)
+
+// console.log("Hello World!")
